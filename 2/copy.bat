223ac4dfaf08cf8ed504b152aa5bd79bd6007a3c
jsmingzi1 <39199489+jsmingzi1@users.noreply.github.com>
2018-06-07 02:45:20 +0530

    clientwinservice update
    with mstsc2, for win server, will not unlock screen, just create new rdp session.
    makre sure always has an explore processing running, or create rdp session will fail

--- 2/copy.bat
+++ 2/copy.bat
@@ -1,4 +1,4 @@
-c:
-cd \2
-copy /y C:\Users\lcm\source\repos\ConsoleApp4\ConsoleApp4\bin\Debug\ConsoleApp4.exe .\ClientWinService.exe
+d:
+cd D:\VS\source\repos\repo1\2
+copy /y D:\VS\source\repos\repos-source\ClientWinService\ConsoleApp4\bin\Debug\ConsoleApp4.exe .\ClientWinService.exe
 pause
